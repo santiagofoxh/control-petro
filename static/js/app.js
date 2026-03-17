8d7d97077cb61b56a399ade341f33fa0677b9f6b
santiagofoxh <buensantiago@gmail.com>
2026-03-17 12:39:18 -0600

    Fix: revert getApiHeaders to use service token only, expired JWT was causing 401s

--- static/js/app.js
+++ static/js/app.js
@@ -127,11 +127,7 @@ fetchServiceToken();
 
 function getApiHeaders() {
   const h = { 'Content-Type': 'application/json' };
-  // Prefer user JWT (set at login), fall back to service token
-  var userTk = null;
-  try { userTk = window['local' + 'Storage']['get' + 'Item']('cp_' + 'token'); } catch(e) {}
-  var tk = userTk || _serviceToken;
-  if (tk) h['Authorization'] = 'Bearer ' + tk;
+  if (_serviceToken) h['Authorization'] = 'Bearer ' + _serviceToken;
   return h;
 }
 
